807b72ef5392868b7d125b6d8621b219332c2850
vladbabinsky <133516479+vladbabinsky@users.noreply.github.com>
2024-11-22 15:45:03 +0200

    Update кубіки.cpp

--- кубіки.cpp
+++ кубіки.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-    srand(time(0));
 
-    int userSum = 0;
-    int compSum = 0;
+    int userSum = 0, compSum = 0;
 
-    cout << "Welcome to the Dice Game" << endl;
+    cout << "Dice Game" << endl;
 
     for (int round = 1; round <= 3; round++) {
-        cout << "\nRound " << round << "" << endl;
+        cout << "\nRound " << round << ":" << endl;
 
         int userRoll1 = rand() % 6 + 1;
         int userRoll2 = rand() % 6 + 1;
-        int userRoundSum = userRoll1 + userRoll2;
-        userSum += userRoundSum;
-        cout << "You rolled: " << userRoll1 << " and " << userRoll2
-            << " (Round total: " << userRoundSum << ")" << endl;
+        userSum += userRoll1 + userRoll2;
+        cout << "You: " << userRoll1 << " + " << userRoll2 
+             << " = " << userRoll1 + userRoll2 << endl;
 
         int compRoll1 = rand() % 6 + 1;
         int compRoll2 = rand() % 6 + 1;
-        int compRoundSum = compRoll1 + compRoll2;
-        compSum += compRoundSum;
-        cout << "Computer rolled: " << compRoll1 << " and " << compRoll2
-            << " (Round total: " << compRoundSum << ")" << endl;
+        compSum += compRoll1 + compRoll2;
+        cout << "Computer: " << compRoll1 << " + " << compRoll2 
+             << " = " << compRoll1 + compRoll2 << endl;
     }
 
-    cout << "\nFinal Scores:" << endl;
-    cout << "Your total: " << userSum << endl;
-    cout << "Computer's total: " << compSum << endl;
+    cout << "\nFinal Score: You = " << userSum 
+         << ", Computer = " << compSum << endl;
 
-    if (userSum > compSum) {
-        cout << "Congratulations, you win" << endl;
-    }
-    else if (userSum < compSum) {
-        cout << "Sorry, the computer wins" << endl;
-    }
-    else {
+    if (userSum > compSum)
+        cout << "You win" << endl;
+    else if (userSum < compSum)
+        cout << "Computer wins" << endl;
+    else
         cout << "It's a tie" << endl;
-    }
-
-    return 0;
 }
